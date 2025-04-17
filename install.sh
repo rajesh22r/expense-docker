9194f77dfdb171b5761b938981481b9c6f2bc42f
rajesh22r <rajesh.raju22698@gmail.com>
2025-04-17 11:40:36 +0530

    k8

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl enable --now docker
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo -y
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl enable --now docker -y
 sudo docker run hello-world
